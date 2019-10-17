46d65e0e2e4b4c510afaf3b11f2b2f0813301bcf
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-10-17 08:14:22 +0700

    selesai

---
+++ src/NewClass.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class NewClass {
+    
+  public static void main(String[] args)  {
+    Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.nim ="10118074";
+        mhs.nama ="Fikri Maulana";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim ="10110271";
+        mhs.nama ="Robi Tansil";
+        
+        mhs.perkenalkanDriri ();
+        
+        mhs.nim ="10110269";
+        mhs.nama ="Muhammad Nur";
+        
+        mhs.perkenalkanDriri ();
+        
+        
+       mhs.nim ="10110270";
+       mhs.nama ="Indra Tiola";
+       
+       mhs.perkenalkanDiri ();
+}
+}
---
+++ src/pbo2/latihan31/mahasiswa/PBO2LATIHAN31Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.latihan31.mahasiswa;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO2LATIHAN31Mahasiswa {
+    
+        
+        public void perkenalkanDiri (){
+    
+     System.out.println("Hello everyone");
+     System.out.println("My NIM :"+nim);
+     System.out.println("My Name : " +nama);
+     System.out.println("");
+    }
+    
+}
